1dd9fe12824664f26cc7bda73b94355805f3e3a7
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 21:38:24 -0700

    feature: create Scoreboard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Scoreboard({p1, p2, p1Score, p2Score}) {
+    return (
+        <div className="scoreboard">
+            <span id="p1" clas="score-panel">{p1} : {p1Score}</span>
+            |
+            <span id="p2" class="score-panel">{p2} : {p2Score}</span>
+        </div>
+    )
+}
